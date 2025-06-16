dc318b7f31c64ef2afd170c034848dbe3c65104f
gusropeca <151777896+gusropeca@users.noreply.github.com>
2025-06-16 20:28:46 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
     import { Start } from './scenes/Start.js';
+    import { TelaMorte } from './scenes/TelaMorte.js';
     import { Fase1 } from './scenes/Fase1.js';
     import { Fase1_1 } from './scenes/Fase1_1.js';
     import { Fase1_2 } from './scenes/Fase1_2.js';
@@ -19,12 +20,12 @@ const config = {
     physics:{
         default: 'arcade',
         arcade:{
-            gravity: { y: 600 },
+            gravity: { y: 500 },
             debug: true,
         }
     },
     scene: [
-        Start, Fase1, Fase1_1, Fase1_2, Fase2, Fase2_1, Fase2_2, Fase2_3
+        Start, TelaMorte, Fase1, Fase1_1, Fase1_2, Fase2, Fase2_1, Fase2_2, Fase2_3
     ],
     scale: {
         mode: Phaser.Scale.FIT,
