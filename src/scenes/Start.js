12c8026a9d3a4c87bc9c574bb93058b0b0239924
gusropeca <151777896+gusropeca@users.noreply.github.com>
2025-07-07 22:17:43 -0300

    Update Start.js

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -7,7 +7,8 @@ export class Start extends Phaser.Scene {
     preload() {
         this.load.image('background', 'assets/CasteloDoMago.jpg');
         this.load.image('logo', 'assets/Titulo.png');
-        this.load.image('pressSpace', 'assets/PressStart.png')
+        this.load.image('pressSpace', 'assets/PressStart.png');
+        this.load.audio('MusicaInicial', 'assets/music/MusicaInicio.mp3');
 
         
     }
@@ -15,22 +16,26 @@ export class Start extends Phaser.Scene {
     create() {
         this.background = this.add.tileSprite(640, 360, 1280, 720, 'background');
 
-        const logo = this.add.image(400, 300, 'logo');
+        const logo = this.add.image(400, 350, 'logo');
+
+          if (!this.sound.get('MusicaInicial')?.isPlaying) {
+            this.sound.add('MusicaInicial', { loop: true, volume: 0.5 }).play();
+        }
+
 
         this.tweens.add({
             targets: logo,
-            y: 250,
+            y: 300,
             duration: 1500,
             ease: 'Sine.inOut',
             yoyo: true,
             loop: -1
         });
 
-        this.spacePrompt = this.add.image(400, 500, 'pressSpace')
+        this.spacePrompt = this.add.image(400, 550, 'pressSpace')
             .setAlpha(0.8)
             .setScale(0.2);
 
-        // Animação de piscar (opcional)
         this.tweens.add({
             targets: this.spacePrompt,
             alpha: 0.3,
@@ -46,7 +51,7 @@ export class Start extends Phaser.Scene {
 
         if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
             this.background.destroy();
-            this.scene.start('Cutscene1'); // Inicia a cutscene
+            this.scene.start('Cutscene1'); 
         }
 
     }
