b299c63266a235d55341f4bee9ab110c48b42f0b
gusropeca <151777896+gusropeca@users.noreply.github.com>
2025-07-07 22:16:01 -0300

    Update Fase2_2.js

--- src/scenes/Fase2_2.js
+++ src/scenes/Fase2_2.js
@@ -1,147 +1,183 @@
-export class Fase2_2 extends Phaser.Scene{
-     constructor() {
-        super('Fase2_2');
+import { CenaBase } from './CenaBase.js';
+import { Cavaleiro } from './Cavaleiro.js'; 
+
+export class Fase2_2 extends CenaBase {
+    constructor() {
+    super('Fase2_2');
+  }
+
+    preload() {
+
+        super.preload();
+        this.load.image('Castelo2', 'assets/InteriorCastelo2.png');
+        this.load.image('CasteloChao', 'assets/CasteloChao.png')
+        this.load.image('PocaoVida', 'assets/PocaoVida.png');
+
     }
 
-        preload() {
-            this.load.image('Castelo3', 'assets/InteriorCastelo2.png');
-            this.load.atlas('magoAtlas', 'assets/sprites/AndarDoMago.png', 'assets/sprites/AndarDoMago.json');
-
-            this.load.image('CasteloChao', 'assets/CasteloChao.png')
-        }
 
 
-        create() {
+  create(data) {
+  
+    this.chao = this.physics.add.staticSprite(400, 300, 'CasteloChao');
+    this.background = this.add.tileSprite(400, 300, 800, 600, 'Castelo2');     
+
+
+        
+    this.criarAnims();
+    this.criarPlayer(data ? data.vida : undefined); 
+    this.criarFireballGroup();
+    this.configurarControles();
+
+    this.inimigos = this.physics.add.group();
+    this.pocoes = this.physics.add.group(); 
+  
+    this.cavaleiros = []; 
+    this.cavaleiros.push(new Cavaleiro(this, 400, 500, this.inimigos, false));
+    this.cavaleiros.push(new Cavaleiro(this, 500, 500, this.inimigos, false));
+    this.cavaleiros.push(new Cavaleiro(this, 600, 500, this.inimigos, true));
+
+    this.physics.add.collider(this.player, this.chao);
+    this.physics.add.collider(this.inimigos, this.chao); 
+
+
+    this.estaAtacando = false;
+
+    this.physics.world.on('worldbounds', (body) => {
+      if (body.gameObject && body.gameObject.texture.key === 'fireball') {
+        body.gameObject.disableBody(true, true);
+      }
+    });
+
+    this.physics.add.overlap(this.fireballs, this.inimigos, (fireball, cavaleiro) => {
+      fireball.disableBody(true, true);
+      cavaleiro.vida -= 20;
+      cavaleiro.setTint(0xff0000);
+      this.time.delayedCall(100, () => cavaleiro.clearTint());
+      if (cavaleiro.vida <= 0) {
+       
+        if (cavaleiro.dropsPotion) {
+            const potion = this.pocoes.create(cavaleiro.x, cavaleiro.y, 'PocaoVida');
             
-            this.cameras.main.fadeIn(1000, 0, 0, 0);
-
-            
-
-            this.chao = this.physics.add.staticImage(400, 300, 'CasteloChao')
-
-            this.background = this.add.tileSprite(400, 300, 800, 600, 'Castelo3');
-
-            this.anims.create({
-                key: 'andarMago',
-                frames: this.anims.generateFrameNames('magoAtlas', {
-                    start: 0,
-                    end: 3,
-                    zeroPad: 0,
-                    prefix: 'AndarDoMago ',
-                    suffix: '.aseprite'
-                }),
-                frameRate: 5,
-                repeat: -1
-            });
-
-             this.anims.create({
-                key: 'PuloMago',
-                frames: this.anims.generateFrameNames('magoPulo', {
-                    start: 1,
-                    end: 5,
-                    zeroPad: 0,
-                    prefix: 'PuloDoMago ',
-                    suffix: '.aseprite'
-                }),
-                frameRate: 5,
-                repeat: 0
-            });
-
-
-            this.player = this.physics.add.sprite(100, 450,'magoAtlas', 'AndarDoMago 0.aseprite');
-            this.player.setScale(2);
-
-
-            this.player.setSize(42, 60);  // Largura, Altura (ajuste conforme seu sprite)
-            this.player.setOffset(0, 14); // Deslocamento X, Y (ajuste conforme necessário)
-
-            this.physics.add.collider(this.player, this.chao);
-
-            this.physics.world.setBounds(0, 0, 1600, 600);
-
-            this.cursors = this.input.keyboard.createCursorKeys();
-
-            this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-            this.player.body.setCollideWorldBounds(true);
-
-
-      
+            potion.setOrigin(0.5, 0.5); 
+            potion.body.setAllowGravity(true); 
+            potion.setCollideWorldBounds(true); 
+            potion.body.setBounceY(0.4); 
+            potion.body.setVelocityY(100); 
         }
+        cavaleiro.destroy(); 
+      }
+    });
+    
 
-        update() {
+    this.physics.add.overlap(this.player, this.pocoes, (player, potion) => {
+        player.vida = Math.min(player.vidaMaxima, player.vida + 60); 
+        this.atualizarHUD(); 
+        potion.destroy(); 
+    });
 
-            const speed = 160;
-            let moving = false;
+    this.physics.add.overlap(this.player, this.inimigos, (player, cavaleiro) => {
+      const now = this.time.now;
+      if (!player.invulneravel && now - cavaleiro.tempoAtaque < 1000) {
+        player.vida -= 10;
+        player.invulneravel = true;
+        cavaleiro.tempoAtaque = now;
+        cavaleiro.play('ataqueCavaleiro', true);
+        this.time.delayedCall(1000, () => (player.invulneravel = false));
+      }
+    });
 
-            if (this.player.x >= this.scale.width - this.player.width / 2) {
-                this.scene.start('Fase2_3');
-        }
+    this.physics.world.setBounds(0, 0, 1600, 600);
+    
+    this.barraVida = this.add.graphics();
+    this.transicionando = false;
+    this.atualizarHUD();
+  }
 
-            // Resetar movimento horizontal
-              this.player.setVelocityX(0);
+  update() {
 
-            if (this.cursors.left.isDown) {
-                this.player.setVelocityX(-speed);
-                this.player.setFlipX(true);
-                this.player.setOffset(20, 14); // ajusta para flip esquerda
-                moving = true;
-            } else if (this.cursors.right.isDown) {
-                this.player.setVelocityX(speed);
-                this.player.setFlipX(false);
-                this.player.setOffset(0, 14); // ajusta para flip direita
-                moving = true;
-            }
+    if (this.player.vida <= 0 && !this.morreu) {
+      this.morreu = true; 
+      this.scene.start('TelaMorte');
+  }
+    const speed = 160;
+    let moving = false;
 
-            if (Phaser.Input.Keyboard.JustDown(this.spaceKey) && this.player.body.onFloor()) {
-                this.player.setVelocityY(-500);
-                this.player.setOffset(7, 14)
-                //this.player.play('PuloMago', true); // Força a animação imediatamente
-            }
-            
-            // Se está NO AR, toca animação de pulo
-            if (!this.player.body.onFloor) {
-                if (this.player.anims.getName() !== 'PuloMago') {
-                this.player.play('PuloMago', true);
-                    }
-            return;
-            // interrompe o resto
-            } 
+    this.player.setVelocityX(0);
 
-            // Se está no chão:
-            if (moving) {
-                if (this.player.anims.getName() !== 'andarMago') {
-                this.player.play('andarMago', true);
-                }
-            } else {
-                this.player.setFrame('AndarDoMago 0.aseprite');
-            }
-
-
-            // Verifica se o player chegou no limite d  ireito
-            if (this.player.x >= this.cameras.main.width) {
-                
-                this.comecarTransicaoParaFase2();
-            }
-         }
-
-
-            comecarTransicaoParaFase2() {
-            // Desativa controles e física
-            this.player.setVelocity(0, 0);
-            this.cursors.left.enabled = false;
-            this.cursors.right.enabled = false;
-            this.spaceKey.enabled = false;
-
-   
-
-            // Aguarda o fade completar e inicia a Fase2
-             this.cameras.main.once('camerafadeoutcomplete', () => {
-                this.scene.start('Fase2_3', {
-                    // Dados persistentes (ex.: vida, itens)
-                    //vida: 100,
-                    //magias: ['fireball']
-                });
-            });
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-speed);
+      this.player.setFlipX(true);
+      this.player.setOffset(20, 14);
+      moving = true;
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(speed);
+      this.player.setFlipX(false);
+      this.player.setOffset(0, 14);
+      moving = true;
+    } else {
+      moving = false;
     }
+
+    if (Phaser.Input.Keyboard.JustDown(this.spaceKey) && this.player.body.onFloor()) {
+      this.player.setVelocityY(-500);
+      this.player.setOffset(7, 14);
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.attackKey)) {
+      this.atacar();
+    }
+
+    if (!this.estaAtacando) {
+      if (moving) {
+        if (this.player.anims.getName() !== 'andarMago') {
+          this.player.play('andarMago', true);
+        }
+      } else {
+        this.player.anims.stop();
+        this.player.setFrame('AndarDoMago 0.aseprite');
+      }
+    }
+
+    this.cavaleiros.forEach(cavaleiroInstance => {
+      cavaleiroInstance.update(this.player);
+    });
+
+    this.atualizarHUD();
+
+    const todosInimigosDerrotados = this.inimigos.countActive(true) === 0;
+    const chegouFim = this.player.x >= this.cameras.main.width;
+
+    if (!this.transicionando && todosInimigosDerrotados && chegouFim) {
+      this.transicionando = true;
+      this.comecarTransicaoParaFase2(); 
+    }
+  }
+
+
+  comecarTransicaoParaFase2() {
+  this.sound.stopByKey('TrilhaSonoraFase2');
+  this.player.setVelocity(0, 0);
+  this.cursors.left.enabled = false;
+  this.cursors.right.enabled = false;
+  this.spaceKey.enabled = false;
+
+  this.cameras.main.once('camerafadeoutcomplete', () => {
+    // Certifique-se que o nome da próxima cena está correto aqui
+    this.scene.start('Fase2_3', { // Pode ser Fase1_2 ou a próxima fase real
+      vida: this.player.vida
+    });
+  });
+
+  this.cameras.main.fadeOut(500); // iniciar fade
+}
+
+
+  atualizarHUD() {
+    this.barraVida.clear();
+    this.barraVida.fillStyle(0x000000);
+    this.barraVida.fillRect(20, 20, 104, 14);
+    this.barraVida.fillStyle(0xff0000);
+    this.barraVida.fillRect(22, 22, Math.max(0, this.player.vida), 10);
+  }
 }
