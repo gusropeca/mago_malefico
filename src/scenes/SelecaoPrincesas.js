8ab088cd1e61f8f4badf422a71e0a39555cfb079
gusropeca <151777896+gusropeca@users.noreply.github.com>
2025-08-01 17:48:28 -0300

    Add files via upload

---
+++ src/scenes/SelecaoPrincesas.js
@@ -0,0 +1,90 @@
+export class SelecaoPrincesas extends Phaser.Scene {
+  constructor() {
+    super('SelecaoPrincesas');
+  }
+
+  preload() {
+    this.load.image('Fundo','assets/SelecaoPrincesa.png');
+
+    this.load.image('princesaAzul', 'assets/princesas/PrincesaAzul.png');
+    this.load.image('princesaLaranja', 'assets/princesas/PrincesaLaranja.png');
+    this.load.image('princesaVerde', 'assets/princesas/PrincesaVerde.png');
+    this.load.image('princesaVermelho', 'assets/princesas/PrincesaVermelha.png');
+    this.load.image('princesaVioleta', 'assets/princesas/PrincesaVioleta.png');
+
+    this.load.image('princesaAzulSel', 'assets/princesas/PrincesaAzulEscolhida.png');
+    this.load.image('princesaLaranjaSel', 'assets/princesas/PrincesaLaranjaEscolhida.png');
+    this.load.image('princesaVerdeSel', 'assets/princesas/PrincesaVerdeEscolhida.png');
+    this.load.image('princesaVermelhoSel', 'assets/princesas/PrincesaVermelhaEscolhida.png');
+    this.load.image('princesaVioletaSel', 'assets/princesas/PrincesaVioleta.png');
+  }
+
+  create() {
+
+    this.background = this.add.tileSprite(400, 300, 800, 600, 'Fundo');     
+
+
+
+    this.princesas = [
+      { nome: 'Azul', x: 200, y: 325 },
+      { nome: 'Laranja', x: 400, y: 325 },
+      { nome: 'Verde', x: 600, y: 325 },
+      { nome: 'Vermelho', x: 300, y: 425 },
+      { nome: 'Violeta', x: 500, y: 425 }
+    ];
+    
+
+    this.imagens = {};
+
+    this.princesas.forEach((p) => {
+      const img = this.add.image(p.x, p.y, `princesa${p.nome}`).setInteractive({ useHandCursor: true });
+      img.setScale(1);
+
+      img.on('pointerover', () => {
+        img.setScale(1.1);
+      });
+
+      img.on('pointerout', () => {
+        if (this.princesaSelecionada !== p.nome) img.setScale(0.8);
+      });
+
+      img.on('pointerdown', () => {
+        this.selecionarPrincesa(p.nome);
+      });
+
+      this.imagens[p.nome] = img;
+    });
+
+    this.instrucao = this.add.text(400, 550, 'Clique em uma princesa para selecionar', {
+      fontSize: '20px',
+      color: '#ffffff',
+      fontFamily: 'Arial'
+    }).setOrigin(0.5);
+
+    this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  }
+
+  selecionarPrincesa(nome) {
+    Object.keys(this.imagens).forEach((key) => {
+      const img = this.imagens[key];
+      if (key === nome) {
+        img.setTexture(`princesa${key}Sel`);
+        img.setScale(1); 
+        this.princesaSelecionada = key;
+      } else {
+        img.setTexture(`princesa${key}`);
+        img.setScale(0.8);
+      }
+    });
+
+    this.instrucao.setText(`Princesa ${nome} selecionada! Pressione ESPAÇO para continuar`);
+  }
+
+  update() {
+    // ✅ Quando o jogador aperta espaço, vai para a cutscene correta
+    if (Phaser.Input.Keyboard.JustDown(this.spaceKey) && this.princesaSelecionada) {
+      this.registry.set('princesaEscolhida', this.princesaSelecionada); 
+      this.scene.start('CutsceneCreditos');
+    }
+  }
+}
