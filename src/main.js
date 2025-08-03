d69e0abf98736e6f2139c9b1bd30e0e28397e01e
Fernando Rodrigues Morais <150272982+Tabolito@users.noreply.github.com>
2025-08-03 15:20:00 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ const config = {
         default: 'arcade',
         arcade:{
             gravity: { y: 500 },
-            debug: true,
+            debug: false,
         }
     },
     scene: [
